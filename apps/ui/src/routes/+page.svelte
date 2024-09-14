30c6fe6a7148ef96b4a0209eae72988507e8949d
austingw <senortinman@gmail.com>
2024-09-14 13:10:54 -0400

    Test out theme

--- apps/ui/src/routes/+page.svelte
+++ apps/ui/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div>
 	<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-	<button class="btn">Click me</button>
+	<button class="btn btn-primary">Click me</button>
 	<h2>Pages</h2>
 </div>
