1755574059c9d8204c76dd82c7bf22fb555e7251
austingw <senortinman@gmail.com>
2024-09-15 14:15:37 -0400

    Add env for server url

--- apps/ui/src/routes/+page.svelte
+++ apps/ui/src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	import { PUBLIC_API_URL } from '$env/static/public';
+	// const webSocket = new WebSocket(PUBLIC_API_URL + "/ws");
+	console.log(PUBLIC_API_URL);
+</script>
+
 <div>
 	<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
