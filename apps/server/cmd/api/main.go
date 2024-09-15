f28e27b154f86b6c8ef651225d48510671922efc
austingw <senortinman@gmail.com>
2024-09-15 14:15:24 -0400

    Add some more setup to main.go

--- apps/server/cmd/api/main.go
+++ apps/server/cmd/api/main.go
@@ -1,14 +1,24 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
 	"flag"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
 
 var addr = flag.String("addr", ":8080", "http service address")
 
@@ -26,13 +36,25 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
+
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
