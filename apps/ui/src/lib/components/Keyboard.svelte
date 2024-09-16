f3a932f0d192550e447e0b7ecaa8c40b6fe781d6
austingw <senortinman@gmail.com>
2024-09-16 18:21:16 -0400

    Pass props/add event handlers

--- apps/ui/src/lib/components/Keyboard.svelte
+++ apps/ui/src/lib/components/Keyboard.svelte
@@ -1,18 +1,22 @@
+<script>
+	let { addLetter, removeLetter, submitAnswer } = $props();
+</script>
+
 <div class="w-full flex flex-col justify-end items-center gap-1">
 	{#each ['qwertyuiop', 'asdfghjkl'] as row}
 		<div class="flex flex-row justify-center items-center gap-1">
 			{#each row as letter}
-				<button class="btn btn-neutral">{letter}</button>
+				<button class="btn btn-neutral" onclick={() => addLetter(letter)}>{letter}</button>
 			{/each}
 		</div>
 	{/each}
 
 	<div class="flex flex-row justify-center items-center gap-1">
-		<button class="btn btn-neutral">Enter</button>
+		<button class="btn btn-neutral" onclick={() => submitAnswer()}>Enter</button>
 		{#each 'zxcvbnm' as letter}
-			<button class="btn btn-neutral">{letter}</button>
+			<button class="btn btn-neutral" onclick={() => addLetter(letter)}>{letter}</button>
 		{/each}
 
-		<button class="btn btn-neutral">← Back</button>
+		<button class="btn btn-neutral" onclick={() => removeLetter()}>← Back</button>
 	</div>
 </div>
