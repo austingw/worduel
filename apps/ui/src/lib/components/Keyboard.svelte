b5f54b2cd0d6f16a235815cfa54e8b23944a1185
austingw <senortinman@gmail.com>
2024-09-16 17:54:49 -0400

    Add keyboard component

---
+++ apps/ui/src/lib/components/Keyboard.svelte
@@ -0,0 +1,18 @@
+<div class="w-full flex flex-col justify-end items-center gap-1">
+	{#each ['qwertyuiop', 'asdfghjkl'] as row}
+		<div class="flex flex-row justify-center items-center gap-1">
+			{#each row as letter}
+				<button class="btn btn-neutral">{letter}</button>
+			{/each}
+		</div>
+	{/each}
+
+	<div class="flex flex-row justify-center items-center gap-1">
+		<button class="btn btn-neutral">Enter</button>
+		{#each 'zxcvbnm' as letter}
+			<button class="btn btn-neutral">{letter}</button>
+		{/each}
+
+		<button class="btn btn-neutral">← Back</button>
+	</div>
+</div>
