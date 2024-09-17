ae4dc650316039d1165c4c80972a3c5b2e43323a
austingw <senortinman@gmail.com>
2024-09-17 00:41:49 -0400

    Add Grid component

---
+++ apps/ui/src/lib/components/Grid.svelte
@@ -0,0 +1,18 @@
+<script>
+	let { letters, attempts } = $props();
+</script>
+
+<div class="flex flex-col items-center justify-center w-full h-full gap-2">
+	{#each attempts as attempt}
+		<div class="flex flex-row items-center justify-center gap-2">
+			{#each attempt as letter}
+				<div class="badge badge-lg badge-ghost text-lg w-12 h-12">{letter}</div>
+			{/each}
+		</div>
+	{/each}
+	<div class="flex flow-row items-center justify-center gap-2">
+		{#each letters as letter}
+			<div class="badge badge-lg text-lg w-12 h-12">{letter}</div>
+		{/each}
+	</div>
+</div>
