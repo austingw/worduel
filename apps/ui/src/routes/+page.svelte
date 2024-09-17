70ca281a5362d340d412ecb7f226f499d8bc40e2
austingw <senortinman@gmail.com>
2024-09-17 00:42:13 -0400

    Refactor some logic

--- apps/ui/src/routes/+page.svelte
+++ apps/ui/src/routes/+page.svelte
@@ -1,26 +1,38 @@
 <script lang="ts">
 	import { PUBLIC_API_URL } from '$env/static/public';
-	import establishWebSocket from '../lib/establishWebSocket.ts';
-	import Keyboard from '../lib/components/Keyboard.svelte';
+	import establishWebSocket from '$lib/establishWebSocket';
+	import Grid from '$lib/components/Grid.svelte';
+	import Keyboard from '$lib/components/Keyboard.svelte';
 	let webSocketEstablished = false;
 	let ws: WebSocket | null = null;
-	let letters: string[] = $state([]);
+	let letters: string[] = $state(['', '', '', '', '']);
+	let cursor: number = $state(0);
 	let answer: string = $derived(letters.join(''));
+	let attempts: string[] = $state([]);
 
 	function addLetter(letter: string) {
-		letters.push(letter);
+		letters[cursor] = letter;
+		cursor = Math.min(Math.max(0, cursor + 1), 4);
 	}
 
 	function removeLetter(letter: string) {
-		letters.pop(letter);
+		letters[cursor] = '';
+		if (cursor > 0) {
+			cursor = Math.min(Math.max(0, cursor - 1), 4);
+		}
 	}
 
 	function submitAnswer() {
-		ws.send(answer);
+		ws?.send(answer);
+		attempts.push(answer);
+		letters = ['', '', '', '', ''];
+		cursor = 0;
 	}
 
+	console.log('state', letters, cursor, answer, attempts);
+
 	$effect(() => {
-		ws = establishWebSocket(webSocketEstablished, ws);
+		ws = establishWebSocket(webSocketEstablished);
 		return () => {
 			if (ws) {
 				ws.close();
@@ -30,7 +42,14 @@
 </script>
 
 <div class="w-full flex flex-col items-center justify-center">
-	<h1 class="text-3xl font-bold underline">Worduel</h1>
-	<button class="btn btn-primary" onclick={() => ws.send('Testing')}>Click me</button>
+	<h1 class="text-3xl font-bold underline pt-20">🖋️Worduel🤺</h1>
+	<h2 class="text-2xl font-bold underline pt-20">Attempts</h2>
+	<Grid {letters} {attempts} />
+	<ul>
+		{#each attempts as attempt}
+			<li>{attempt}</li>
+		{/each}
+	</ul>
+	<button class="btn btn-primary" onclick={() => ws?.send('Testing')}>Click me</button>
 	<Keyboard {addLetter} {removeLetter} {submitAnswer} />
 </div>
