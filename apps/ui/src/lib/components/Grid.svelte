a190466e7adf800ca52545363a5a9386f6633f0d
austingw <senortinman@gmail.com>
2024-09-17 19:48:25 -0400

    Add logic for blank squares

--- apps/ui/src/lib/components/Grid.svelte
+++ apps/ui/src/lib/components/Grid.svelte
@@ -1,5 +1,10 @@
-<script>
-	let { letters, attempts } = $props();
+<script lang="ts">
+	let { letters, attempts }: { letters: string[]; attempts: string[] } = $props();
+	let blanks = $derived.by<string[]>(() => {
+		if (letters.length < 5) {
+			return Array(5 - letters.length).fill('');
+		}
+	});
 </script>
 
 <div class="flex flex-col items-center justify-center w-full h-full gap-2">
@@ -13,6 +18,9 @@
 	<div class="flex flow-row items-center justify-center gap-2">
 		{#each letters as letter}
 			<div class="badge badge-lg text-lg w-12 h-12">{letter}</div>
+		{/each}
+		{#each blanks as blank}
+			<div class="badge badge-lg text-lg w-12 h-12">{blank}</div>
 		{/each}
 	</div>
 </div>
