739db7f32b68dfde7fc07e12c834b89ae6a2f2a5
austingw <senortinman@gmail.com>
2024-09-17 00:41:59 -0400

    Minor updates for ws util

--- apps/ui/src/lib/establishWebSocket.ts
+++ apps/ui/src/lib/establishWebSocket.ts
@@ -1,19 +1,20 @@
-export default function establishWebSocket (webSocketEstablished: boolean) {
-		if (webSocketEstablished) return;
-		const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-		const newWs = new WebSocket(`${protocol}//localhost:4000/ws`);
+export default function establishWebSocket(webSocketEstablished: boolean) {
+	if (webSocketEstablished) {
+		return null;
+	}
+	const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+	const newWs = new WebSocket(`${protocol}//localhost:4000/ws`);
+	webSocketEstablished = true;
+
+	newWs.addEventListener('open', (event) => {
 		webSocketEstablished = true;
-
-		newWs.addEventListener('open', (event) => {
-			webSocketEstablished = true;
-			console.log('[websocket] connection open', event);
-		});
-		newWs.addEventListener('close', (event) => {
-			console.log('[websocket] connection closed', event);
-		});
-		newWs.addEventListener('message', (event) => {
-			console.log('[websocket] message received', event);
-		});
-  return newWs;
-};
-
+		console.log('[websocket] connection open', event);
+	});
+	newWs.addEventListener('close', (event) => {
+		console.log('[websocket] connection closed', event);
+	});
+	newWs.addEventListener('message', (event) => {
+		console.log('[websocket] message received', event);
+	});
+	return newWs;
+}
