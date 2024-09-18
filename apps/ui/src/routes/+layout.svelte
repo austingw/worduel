34eb79f24e2e2053457c0ab9e0651dc67e9d2c66
austingw <senortinman@gmail.com>
2024-09-17 20:05:39 -0400

    Update deprecated method

--- apps/ui/src/routes/+layout.svelte
+++ apps/ui/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang="ts">
+	import type { Snippet } from 'svelte';
 	import '../app.css';
+	let { children }: { children: Snippet } = $props()
 </script>
 
 <div class="w-full flex flex-col items-center justify-center">
-	<slot />
+	{@render children()}
 </div>
