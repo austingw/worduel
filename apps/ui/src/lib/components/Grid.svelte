044d9dbe8004d1c3331427f170c12ce271cb957e
austingw <senortinman@gmail.com>
2024-09-19 22:48:50 -0400

    Add function for attempt styling

--- apps/ui/src/lib/components/Grid.svelte
+++ apps/ui/src/lib/components/Grid.svelte
@@ -12,13 +12,25 @@
 		}
 		return [];
 	});
+
+	function styleLetter(attemptChar: string, answerChar: string) {
+		if (attemptChar === answerChar) {
+			return 'badge-success';
+		} else if (answer.includes(attemptChar)) {
+			return 'badge-warning';
+		} else {
+			return 'badge-ghost';
+		}
+	}
 </script>
 
 <div class="flex flex-col items-center justify-center w-full h-full gap-2">
 	{#each attempts as attempt}
 		<div class="flex flex-row items-center justify-center gap-2">
-			{#each attempt as letter}
-				<div class="badge badge-lg badge-ghost text-lg w-12 h-12">{letter}</div>
+			{#each attempt as letter, index}
+				<div class={`badge badge-lg ${styleLetter(letter, answer[index])} text-lg w-12 h-12`}>
+					{letter}
+				</div>
 			{/each}
 		</div>
 	{/each}
