6ae09449dd5ec8adb0196a5229fffb2b71dd78b0
austingw <senortinman@gmail.com>
2024-09-19 23:00:44 -0400

    Additional button tweaks

--- apps/ui/src/lib/components/Keyboard.svelte
+++ apps/ui/src/lib/components/Keyboard.svelte
@@ -12,22 +12,29 @@
 	{#each ['qwertyuiop', 'asdfghjkl'] as row}
 		<div class="flex flex-row justify-center items-center gap-1">
 			{#each row as letter}
-				<button class="btn btn-neutral btn-sm sm:btn-md" onclick={() => addLetter(letter)}
-					>{letter}</button
+				<button
+					class="btn btn-circle btn-neutral btn-sm sm:btn-md"
+					onclick={() => addLetter(letter)}>{letter}</button
 				>
 			{/each}
 		</div>
 	{/each}
 
 	<div class="flex flex-row justify-center items-center gap-1">
-		<button class="btn btn-primary btn-sm sm:btn-md" onclick={() => submitAnswer()}>Enter</button>
+		<button
+			class="btn btn-circle btn-secondary btn-sm sm:btn-md text-[.5rem] sm:text-sm"
+			onclick={() => submitAnswer()}>Enter</button
+		>
 		{#each 'zxcvbnm' as letter}
-			<button class="btn btn-neutral btn-sm sm:btn-md" onclick={() => addLetter(letter)}
+			<button class="btn btn-circle btn-neutral btn-sm sm:btn-md" onclick={() => addLetter(letter)}
 				>{letter}</button
 			>
 		{/each}
 
-		<button class="btn btn-secondary btn-sm sm:btn-md" onclick={() => removeLetter()}>← Back</button
-		>
+		<button
+			class="btn btn-circle text-lg btn-primary btn-sm sm:btn-md"
+			onclick={() => removeLetter()}
+			>←
+		</button>
 	</div>
 </div>
