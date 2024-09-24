f8484a42d2e28dd79b16b9ae141a188a5684a8cd
austingw <senortinman@gmail.com>
2024-09-23 21:28:53 -0400

    Update main

--- apps/server/cmd/api/main.go
+++ apps/server/cmd/api/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
-	"worduel/internal/data"
-
-	"github.com/coder/websocket"
-	"github.com/coder/websocket/wsjson"
 )
 
 const version = "1.0.0"
@@ -35,51 +29,15 @@ func main() {
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	// app := &application{
-	// 	config: cfg,
-	// 	logger: logger,
-	// }
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		c, err := websocket.Accept(w, r, &websocket.AcceptOptions{
-			OriginPatterns: []string{"localhost:5173"},
-		})
-		if err != nil {
-			logger.Error(err.Error())
-		}
-		defer c.CloseNow()
-
-		// Set the context as needed. Use of r.Context() is not recommended
-		// to avoid surprising behavior (see http.Hijacker).
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		for {
-			var input data.Message
-			err = wsjson.Read(ctx, c, &input)
-
-			if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
-				return
-			}
-			if websocket.CloseStatus(err) == websocket.StatusNormalClosure ||
-				websocket.CloseStatus(err) == websocket.StatusGoingAway {
-				return
-			}
-			if err != nil {
-				logger.Error(err.Error())
-			}
-
-			err = wsjson.Write(ctx, c, input)
-			if err != nil {
-				logger.Error(err.Error())
-			}
-		}
-	})
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
 
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
