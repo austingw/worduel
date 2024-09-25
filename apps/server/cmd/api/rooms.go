fab978d0ff5f2c558099ba73a6fbbe0f071d8c46
austingw <senortinman@gmail.com>
2024-09-24 22:49:56 -0400

    Implement test response

--- apps/server/cmd/api/rooms.go
+++ apps/server/cmd/api/rooms.go
@@ -1,10 +1,24 @@
 package main
 
-func (app *application) createRoomHandler() {
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *application) createRoomHandler(w http.ResponseWriter, r *http.Request) {
 }
 
-func (app *application) joinRoomHandler() {
+func (app *application) joinRoomHandler(w http.ResponseWriter, r *http.Request) {
 }
 
-func (app *application) listRoomsHandler() {
+func (app *application) listRoomsHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"hello": "world",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
