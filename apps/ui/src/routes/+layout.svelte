624cc0f4e50a92a1b44df2a7ed787027fabd435e
austingw <senortinman@gmail.com>
2024-09-25 21:26:07 -0400

    Implement query client

--- apps/ui/src/routes/+layout.svelte
+++ apps/ui/src/routes/+layout.svelte
@@ -2,9 +2,16 @@
   import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
 	import type { Snippet } from 'svelte';
 	import '../app.css';
+	import { browser } from '$app/environment';
 
 	let { children }: { children: Snippet } = $props()
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+  defaultOptions: {
+      queries: {
+        enabled: browser,
+      },
+    },
+  })
 </script>
 
 
