aeb4e27f3a50841eadc9ffbad2ace5cafd4440f3
austingw <senortinman@gmail.com>
2024-09-25 21:25:13 -0400

    Add CORS middleware

---
+++ apps/server/cmd/api/middleware.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		next.ServeHTTP(w, r)
+	})
+}
