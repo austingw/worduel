5753a4d427135fc8c30a94a5ced56d543b86ef18
austingw <senortinman@gmail.com>
2024-09-25 21:26:22 -0400

    Add a test query

--- apps/ui/src/lib/components/RoomList.svelte
+++ apps/ui/src/lib/components/RoomList.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+	import { env } from '$env/dynamic/public';
+	import { createQuery } from '@tanstack/svelte-query';
+
+	const query = createQuery(() => ({
+		queryKey: ['rooms'],
+		queryFn: async () => {
+			const response = await fetch(`${env.PUBLIC_API_URL}/rooms`);
+			if (!response.ok) {
+				throw new Error('Failed to fetch rooms');
+			}
+			return await response.json();
+		}
+	}));
 </script>
 
 <table class="table table-zebra">
@@ -12,3 +25,5 @@
 	</thead>
 	<tbody> </tbody>
 </table>
+
+<h3>{query.data?.hello}</h3>
