16831ac6151944b7d9c47ceeaba79fa6c34f379d
austingw <senortinman@gmail.com>
2024-09-25 21:25:35 -0400

    Implement middleware

--- apps/server/cmd/api/routes.go
+++ apps/server/cmd/api/routes.go
@@ -5,10 +5,11 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/ws", app.websocketHandler)
-	mux.HandleFunc("GET /rooms", app.listRoomsHandler)
+	mux.Handle("/ws", app.enableCORS(http.HandlerFunc(app.websocketHandler)))
+	mux.Handle("GET /rooms", app.enableCORS(http.HandlerFunc(app.listRoomsHandler)))
 	mux.HandleFunc("POST /rooms", app.createRoomHandler)
 	mux.HandleFunc("PUT /rooms", app.joinRoomHandler)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
 
 	return mux
 }
