b7130ea2c1569d4dda00a043f7fc0b48636be795
austingw <senortinman@gmail.com>
2024-09-26 22:02:52 -0400

    Add general idea for room list component

--- apps/ui/src/lib/components/RoomList.svelte
+++ apps/ui/src/lib/components/RoomList.svelte
@@ -4,16 +4,36 @@
 	const query = getRooms();
 </script>
 
-<table class="table table-zebra">
-	<thead>
-		<tr>
-			<th></th>
-			<th class="flex flex-grow">Room</th>
-			<th># of Players</th>
-			<th>Join?</th>
-		</tr>
-	</thead>
-	<tbody> </tbody>
-</table>
+<div class="flex flex-col items-center justify-center">
+	{#if query.isLoading}
+		<span class="loading loading-spinner loading-lg text-secondary"></span>
+	{:else if query.data}
+		<table class="table table-zebra">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th class="flex flex-grow">Room</th>
+					<th># of Players</th>
+					<th>Join?</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each query.data.rooms as room, index}
+					<tr>
+						<td>{index + 1}</td>
+						<td>{room.name}</td>
+						<td>{room.players.length}</td>
+						<td>
+							<button
+								class={`btn btn-secondary ${room.players.length > 1 && 'btn-disabled'}`}
+								onclick={() => console.log('joins room')}>Join</button
+							>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{/if}
+</div>
 
 <h3>{query.data?.hello}</h3>
