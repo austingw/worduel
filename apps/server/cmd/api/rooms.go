1aa9c118ff0d8a09a6d8ec1e00a063363499c245
austingw <senortinman@gmail.com>
2024-09-28 16:38:59 -0400

    Add logic for joining a room

--- apps/server/cmd/api/rooms.go
+++ apps/server/cmd/api/rooms.go
@@ -15,6 +15,7 @@ func (app *application) createRoomHandler(w http.ResponseWriter, r *http.Request
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusBadRequest)
+		return
 	}
 
 	newRoom := Room{
@@ -26,6 +27,39 @@ func (app *application) createRoomHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (app *application) joinRoomHandler(w http.ResponseWriter, r *http.Request) {
+	app.mu.Lock()
+	defer app.mu.Unlock()
+
+	var input struct {
+		Name string `json:"name"`
+		User User   `json:"user"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusBadRequest)
+		return
+	}
+
+	room, ok := app.rooms[input.Name]
+	if !ok {
+		app.logger.Error(err.Error())
+		http.Error(w, "Room not found", http.StatusNotFound)
+		return
+	}
+
+	if room.Users[0].Name == "" {
+		room.Users[0] = input.User
+	} else if room.Users[1].Name == "" {
+		room.Users[1] = input.User
+	} else {
+		http.Error(w, "Room is full", http.StatusConflict)
+		return
+	}
+
+	app.rooms[input.Name] = room
+	w.WriteHeader(http.StatusOK)
 }
 
 func (app *application) listRoomsHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,5 +67,6 @@ func (app *application) listRoomsHandler(w http.ResponseWriter, r *http.Request)
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
 	}
 }
