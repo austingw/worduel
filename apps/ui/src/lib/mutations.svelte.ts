dc35cda8e85a8122af7c502bb2ea97be84acfe63
austingw <senortinman@gmail.com>
2024-09-28 18:11:26 -0400

    Add createRoom mutation

---
+++ apps/ui/src/lib/mutations.svelte.ts
@@ -0,0 +1,19 @@
+import { env } from "$env/dynamic/public";
+import { createMutation } from "@tanstack/svelte-query";
+
+export const createRoom = () => {
+  return createMutation(() => ({
+    mutationKey: ['createRoom'],
+    mutationFn: async (name: string) => {
+      const response = await fetch(`${env.PUBLIC_API_URL}/rooms`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name }),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to create room');
+      }
+      return await response.json();
+    }
+  }))
+}
