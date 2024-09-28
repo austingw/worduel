ff56dd0b4589a0d72ad0542674069c744326cbc9
austingw <senortinman@gmail.com>
2024-09-28 18:09:59 -0400

    Add a return for create rooms for now

--- apps/server/cmd/api/rooms.go
+++ apps/server/cmd/api/rooms.go
@@ -24,6 +24,12 @@ func (app *application) createRoomHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	app.rooms[input.Name] = newRoom
+	err = app.writeJSON(w, http.StatusOK, envelope{"rooms": app.rooms}, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) joinRoomHandler(w http.ResponseWriter, r *http.Request) {
