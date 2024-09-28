5ae4dec7d60d38b8a6b12f894be90aa63110739a
austingw <senortinman@gmail.com>
2024-09-28 16:08:17 -0400

    Fill out logic for creating a room

--- apps/server/cmd/api/rooms.go
+++ apps/server/cmd/api/rooms.go
@@ -5,6 +5,24 @@ import (
 )
 
 func (app *application) createRoomHandler(w http.ResponseWriter, r *http.Request) {
+	app.mu.Lock()
+	defer app.mu.Unlock()
+	var input struct {
+		Name string `json:"name"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusBadRequest)
+	}
+
+	newRoom := Room{
+		Name:  input.Name,
+		Users: [2]User{},
+	}
+
+	app.rooms[input.Name] = newRoom
 }
 
 func (app *application) joinRoomHandler(w http.ResponseWriter, r *http.Request) {
