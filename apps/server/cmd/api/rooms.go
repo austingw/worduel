3d7bded586a3f195f5585f5961b06015c08b77c2
austingw <senortinman@gmail.com>
2024-09-28 15:49:15 -0400

    Refactor rooms to use new util

--- apps/server/cmd/api/rooms.go
+++ apps/server/cmd/api/rooms.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -12,11 +11,7 @@ func (app *application) joinRoomHandler(w http.ResponseWriter, r *http.Request) 
 }
 
 func (app *application) listRoomsHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"hello": "world",
-	}
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(data)
+	err := app.writeJSON(w, http.StatusOK, envelope{"rooms": app.rooms}, nil)
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
