f13d813c67ba50bcc87713521752f59cf98027a7
austingw <senortinman@gmail.com>
2024-10-03 22:09:07 -0400

    Make new component for room history

---
+++ apps/ui/src/lib/components/History.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { getMessages } from '$lib/websocket.svelte';
+	let msgs = $state(getMessages());
+
+	$effect(() => {
+		msgs = getMessages();
+	});
+</script>
+
+<div class="flex flex-col items-center justify-center w-2/5">
+	<div class="collapse bg-base-200">
+		<input type="checkbox" />
+		<div class="collapse-title text-xl font-medium">Click me to show/hide content</div>
+		<div class="collapse-content">
+			{#each msgs as msg}
+				<p>{msg}</p>
+			{/each}
+		</div>
+	</div>
+</div>
