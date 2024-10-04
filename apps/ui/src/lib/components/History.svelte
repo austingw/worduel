ae411716b3d569788048ea0b4b764d0ed8890dc3
austingw <senortinman@gmail.com>
2024-10-03 22:14:17 -0400

    Add mobile width

--- apps/ui/src/lib/components/History.svelte
+++ apps/ui/src/lib/components/History.svelte
@@ -7,7 +7,7 @@
 	});
 </script>
 
-<div class="flex flex-col items-center justify-center w-2/5">
+<div class="flex flex-col items-center justify-center w-11/12 sm:w-2/5">
 	<div class="collapse bg-base-200">
 		<input type="checkbox" />
 		<div class="collapse-title text-xl font-medium">Click me to show/hide content</div>
