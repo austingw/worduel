b5d447c57a66b6e11ab17c17500f3a76e1e1e65b
austingw <senortinman@gmail.com>
2024-10-03 22:21:05 -0400

    Add bg color

--- apps/ui/src/lib/components/History.svelte
+++ apps/ui/src/lib/components/History.svelte
@@ -7,10 +7,10 @@
 	});
 </script>
 
-<div class="flex flex-col items-center justify-center w-11/12 sm:w-2/5">
-	<div class="collapse bg-base-200">
+<div class="flex flex-col bg-base-200 items-center justify-center w-11/12 sm:w-2/5">
+	<div class="collapse collapse-plus">
 		<input type="checkbox" />
-		<div class="collapse-title text-xl font-medium">Click me to show/hide content</div>
+		<div class="collapse-title text-xl font-medium">Room History</div>
 		<div class="collapse-content">
 			{#each msgs as msg}
 				<p>{msg}</p>
