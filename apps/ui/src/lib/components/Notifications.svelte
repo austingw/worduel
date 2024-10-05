58d0910023d8a4b1c11e0b5e3eaec0567e6f37aa
austingw <senortinman@gmail.com>
2024-10-05 12:12:59 -0400

    Update history component to notifications

--- apps/ui/src/lib/components/Notifications.svelte
+++ apps/ui/src/lib/components/Notifications.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { getMessages } from '$lib/websocket.svelte';
+	import { getMessages, getShowNotification } from '$lib/websocket.svelte';
 	let msgs = $state(getMessages());
 
 	$effect(() => {
@@ -7,12 +7,19 @@
 	});
 </script>
 
+{#if getShowNotification()}
+	<div role="alert" class="alert bg-secondary absolute bottom-4 right-4 w-fit h-fit animate-bounce">
+		<p>{msgs[0]}</p>
+	</div>
+{/if}
+
 <div class="flex flex-col bg-base-200 items-center justify-center w-11/12 sm:w-2/5">
 	<div class="collapse collapse-plus">
 		<input type="checkbox" />
-		<div class="collapse-title text-xl font-medium">Room History</div>
+		<div class="collapse-title text-xl font-medium">View Notifications</div>
 		<div class="collapse-content">
 			{#each msgs as msg}
+				<div class="divider"></div>
 				<p>{msg}</p>
 			{/each}
 		</div>
