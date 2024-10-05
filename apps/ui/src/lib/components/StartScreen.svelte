ec281b72ff2ae7a5995141bff580bb1d1f2a46bb
austingw <senortinman@gmail.com>
2024-10-05 13:48:08 -0400

    Create a start screen for name input

---
+++ apps/ui/src/lib/components/StartScreen.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Screen } from '$lib/types';
+
+	type StartScreenProps = {
+		name: string;
+		changeScreen: (newScreen: Screen) => void;
+	};
+
+	let { name, changeScreen }: StartScreenProps = $props();
+</script>
+
+<div class="flex flex-row items-center justify-between">
+	<input
+		type="text"
+		bind:value={name}
+		placeholder="Enter your name..."
+		class="input input-bordered"
+	/>
+	<button
+		class="btn btn-secondary"
+		onclick={() => {
+			changeScreen('list');
+		}}>Confirm</button
+	>
+</div>
