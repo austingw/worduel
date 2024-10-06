8c12b887b32183a33c1c8a9b2f602daa2ad35574
austingw <senortinman@gmail.com>
2024-10-06 15:42:38 -0400

    Modify layout

--- apps/ui/src/routes/+layout.svelte
+++ apps/ui/src/routes/+layout.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
-  import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import type { Snippet } from 'svelte';
 	import '../app.css';
 	import { browser } from '$app/environment';
 
-	let { children }: { children: Snippet } = $props()
-  const queryClient = new QueryClient({
-  defaultOptions: {
-      queries: {
-        enabled: browser,
-      },
-    },
-  })
+	let { children }: { children: Snippet } = $props();
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser
+			}
+		}
+	});
 </script>
 
-
 <QueryClientProvider client={queryClient}>
-  <div class="w-full flex flex-col items-center justify-center">
-    {@render children()}
-  </div>
+	<div class="w-screen h-screen flex flex-col items-center justify-center">
+		{@render children()}
+	</div>
 </QueryClientProvider>
