338909c229fb74a4ab21c7a83f1ca28d50e34375
austingw <senortinman@gmail.com>
2024-10-13 11:33:04 -0400

    Add method to generate new word

--- apps/server/cmd/api/game.go
+++ apps/server/cmd/api/game.go
@@ -1,6 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"math/rand"
+	"net/http"
+	words "worduel/internal"
+)
 
 func (app *application) checkAnswer(w http.ResponseWriter, r *http.Request) {
+}
+
+func (app *application) newWord() string {
+	return words.List[rand.Intn(len(words.List))]
 }
