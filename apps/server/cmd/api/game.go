96c9b88fde51ecea604ac312a4a1a8772da1b8e3
austingw <senortinman@gmail.com>
2024-10-14 22:07:49 -0400

    Add check answer method

--- apps/server/cmd/api/game.go
+++ apps/server/cmd/api/game.go
@@ -1,8 +1,52 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"errors"
+	"time"
+	words "worduel/internal"
+
+	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
 )
 
-func (app *application) checkAnswer(w http.ResponseWriter, r *http.Request) {
+func (app *application) checkAnswer(attempt Message, conn *websocket.Conn) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	room, ok := app.rooms[attempt.Room]
+	if !ok {
+		return errors.New("Room does not exist")
+	}
+
+	if attempt.Content != room.CurrentWord {
+		for _, user := range room.Users {
+			if user.Ws != conn {
+				err := wsjson.Write(ctx, user.Ws, envelope{"message": attempt.Username + " just made a guess!"})
+				if err != nil {
+					app.logger.Error(err.Error())
+				}
+			}
+		}
+	}
+
+	if attempt.Content == room.CurrentWord {
+		for i, user := range room.Users {
+			if user.Ws == conn {
+				err := wsjson.Write(ctx, user.Ws, envelope{"type": "end", "message": "You won!"})
+				if err != nil {
+					app.logger.Error(err.Error())
+				}
+				room.Users[i].Score++
+			} else {
+				err := wsjson.Write(ctx, user.Ws, envelope{"type": "end", "message": attempt.Username + " won!"})
+				if err != nil {
+					app.logger.Error(err.Error())
+				}
+			}
+		}
+		room.CurrentWord = words.NewWord()
+		app.rooms[room.Name] = room
+	}
+	return nil
 }
