103e80f017f3a3b1c219a2d71afb44e56c23379f
austingw <senortinman@gmail.com>
2024-10-19 13:10:54 -0400

    Add footer component

---
+++ apps/ui/src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<footer class="footer flex-col justify-start bg-neutral text-neutral-content p-10">
+	<aside>
+		<p class="font-bold">
+			Created by <a class="underline" href="https://austingw.com">austingw </a>
+		</p>
+		<p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+	</aside>
+</footer>
