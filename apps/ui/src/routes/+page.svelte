6ffed8ca61757ad13e8ca966a16716a360df2b3c
austingw <senortinman@gmail.com>
2024-10-19 13:11:14 -0400

    Add footer

--- apps/ui/src/routes/+page.svelte
+++ apps/ui/src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Room from '$lib/components/Room.svelte';
 	import RoomList from '$lib/components/RoomList.svelte';
 	import Start from '$lib/components/Start.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 	import type { View } from '$lib/types';
 
 	let name = $state<string>('');
@@ -24,16 +25,21 @@
 	});
 </script>
 
-<div class="w-full max-w-screen h-screen flex flex-col items-center justify-start pt-12">
-	<article class="prose text-center">
-		<h1 class="underline">Worduel</h1>
-	</article>
+<div
+	class="w-full max-w-screen max-full min-h-screen h-full flex flex-col items-center justify-between pt-12"
+>
+	<div class="flex flex-col w-full h-full items-center justify-start">
+		<article class="prose text-center">
+			<h1 class="underline">Worduel</h1>
+		</article>
 
-	{#if view === 'start'}
-		<Start {inputName} {changeView} />
-	{:else if view === 'list'}
-		<RoomList {name} {changeView} />
-	{:else if view === 'room'}
-		<Room {name} {changeView} />
-	{/if}
+		{#if view === 'start'}
+			<Start {inputName} {changeView} />
+		{:else if view === 'list'}
+			<RoomList {name} {changeView} />
+		{:else if view === 'room'}
+			<Room {name} {changeView} />
+		{/if}
+	</div>
+	<Footer />
 </div>
