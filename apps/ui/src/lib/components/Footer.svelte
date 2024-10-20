11e086a573d01c0445b3bf4e5649b604d3f62a70
austingw <senortinman@gmail.com>
2024-10-20 12:03:56 -0400

    Minor copy fixes

--- apps/ui/src/lib/components/Footer.svelte
+++ apps/ui/src/lib/components/Footer.svelte
@@ -1,8 +1,8 @@
 <footer class="footer flex-col justify-start bg-neutral text-neutral-content p-10">
 	<aside>
 		<p class="font-bold">
-			Created by <a class="underline" href="https://austingw.com">austingw </a>
+			Created by <a class="underline" href="https://austingw.com">austingw</a>
 		</p>
-		<p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+		<p>Copyright © {new Date().getFullYear()} - All rights reserved</p>
 	</aside>
 </footer>
